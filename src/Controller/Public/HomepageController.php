8fca1d2633f459d0cdced21f3403a20afbb6dde2
N1Code7 <n1code7@outlook.fr>
2022-09-28 19:05:32 +0200

    :construction: display one article when click on it

--- src/Controller/Public/HomepageController.php
+++ src/Controller/Public/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Public;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage_index')]
-    public function index(): Response
+    public function index(ArticleRepository $repository): Response
     {
+        $articles = $repository->findLast10();
+
         return $this->render('public/homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
+            "articles" => $articles,
+        ]);
+    }
+
+    #[Route("/article/{id}", name: "app_homepage_displayArticle")]
+    public function displayArticle(ArticleRepository $repository, int $id): Response
+    {
+        $article = $repository->find($id);
+
+        return $this->render("public/homepage/displayArticle.html.twig", [
+            "article" => $article,
         ]);
     }
 }
