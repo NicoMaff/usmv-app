b9903025c0cfd04bf43bb1b852b61855f4165b7a
N1Code7 <n1code7@outlook.fr>
2022-10-03 16:46:46 +0200

    :sparkles: :lipstick: add articles pagination when display all articles

--- src/Controller/Public/HomepageController.php
+++ src/Controller/Public/HomepageController.php
@@ -3,9 +3,11 @@
 namespace App\Controller\Public;
 
 use App\Repository\ArticleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomepageController extends AbstractController
 {
@@ -30,12 +32,32 @@ class HomepageController extends AbstractController
     }
 
     #[Route("/articles", name: "app_homepage_displayAllArticles")]
-    public function displayAllArticles(ArticleRepository $repository): Response
+    public function displayAllArticles(ArticleRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
         $articles = $repository->findAll();
 
+        $pagination = $paginator->paginate(
+            $articles,
+            $request->query->getInt("page", 1),
+            12
+        );
+
         return $this->render("public/homepage/displayAllArticles.html.twig", [
-            "articles" => $articles
+            "articles" => $articles,
+            "pagination" => $pagination
         ]);
     }
 }
+
+
+// $donnees = $this->getDoctrine()->getRepository(Articles::class)->findBy([], ['created_at' => 'desc']);
+
+// $articles = $paginator->paginate(
+//     $donnees, // Requête contenant les données à paginer (ici nos articles)
+//     $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+//     6 // Nombre de résultats par page
+// );
+
+// return $this->render('articles/index.html.twig', [
+//     'articles' => $articles,
+// ]);
