c02887e49b10d716fdae13c6edc58930eda5bb69
N1Code7 <n1code7@outlook.fr>
2022-11-08 20:09:49 +0100

    :hammer: add User API

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -27,7 +27,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 100)]
     #[Groups(["user:read", "user:create", "user:update"])]
-    private array $roles = ["ROLE_USER"];
+    private array $roles = ["ROLE_MEMBER"];
 
     /**
      * @var string The hashed password
