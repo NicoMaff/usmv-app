d322ec50e4e5fe8ff35e6f7b9acc6b58e31dd002
N1Code7 <n1code7@outlook.fr>
2022-11-09 13:19:27 +0100

    :wrench: set logout path

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,17 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Repository\UserRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Security\Http\Attribute\CurrentUser;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Serializer\Exception\NotEncodableValueException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -35,53 +27,9 @@ class SecurityController extends AbstractController
         ]);
     }
 
-    // #[Route("/api/login", "api_security_login")]
-    // public function apiLogin(#[CurrentUser] ?User $user): Response
-    // {
-    //     if ($user === null) {
-    //         return $this->json([
-    //             "message" => "missing credentials",
-    //         ], Response::HTTP_UNAUTHORIZED);
-    //     }
-
-    //     $token = "10";
-
-    //     return $this->json([
-    //         "user" => $user->getUserIdentifier(),
-    //         "token" => $token
-    //     ]);
-    // }
-
-    // #[Route(path: '/logout', name: 'app_security_logout')]
-    // public function logout(): void
-    // {
-    // throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    // return $this->redirectToRoute("app_homepage_index");
-    // }
-
-    // #[Route("/api/registration", "api_security_registration")]
-    // public function registration(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, UserRepository $repository, UserPasswordHasherInterface $hasher): Response
-    // {
-    //     $jsonReceived = $request->getContent();
-    //     // dd($jsonReceived);
-
-    //     $user = $serializer->deserialize($jsonReceived, USER::class, "json");
-    //     // dd($user);
-    //     $user->setCreatedAt(new \DateTimeImmutable());
-
-    //     $hashedPassword = $hasher->hashPassword($user, $user->getPassword());
-    //     $user->setPassword($hashedPassword);
-
-    //     $user->setValidatedAccount(false);
-
-    //     $errors = $validator->validate($user);
-
-    //     if (count($errors) > 0) {
-    //         return $this->json($errors, 400);
-    //     }
-
-    //     $repository->add($user, true);
-
-    //     return $this->json($user, 201, [], []);
-    // }
+    #[Route(path: '/logout', name: 'app_security_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
 }
