207c72446825910f76dde654e92b7981aa32ba60
N1Code7 <n1code7@outlook.fr>
2023-01-10 11:58:41 +0100

    :hammer: update tournament PATCH route to allow to only modify the file

--- src/Controller/API/ApiTournamentController.php
+++ src/Controller/API/ApiTournamentController.php
@@ -121,75 +121,78 @@ class ApiTournamentController extends AbstractController
         } else {
             $deleteFile = false;
         }
-
+        // dd($jsonReceived);
         $tournament = $repository->find($id);
-        $updatedTournament = $serializer->deserialize($jsonReceived, Tournament::class, "json");
 
-        if ($updatedTournament->getName()) {
-            $tournament->setName($updatedTournament->getName());
-        }
-        if ($updatedTournament->getCity()) {
-            $tournament->setCity($updatedTournament->getCity());
-        }
-        if ($updatedTournament->getStartDate()) {
-            $tournament->setStartDate($updatedTournament->getStartDate());
-        }
-        if ($updatedTournament->getEndDate()) {
-            $tournament->setEndDate($updatedTournament->getEndDate());
-        }
-        if ($updatedTournament->getStandardPrice1()) {
-            $tournament->setStandardPrice1($updatedTournament->getStandardPrice1());
-        }
-        if ($updatedTournament->getStandardPrice2()) {
-            $tournament->setStandardPrice2($updatedTournament->getStandardPrice2());
-        }
-        if ($updatedTournament->getStandardPrice3()) {
-            $tournament->setStandardPrice3($updatedTournament->getStandardPrice1());
-        }
-        if ($updatedTournament->getElitePrice1()) {
-            $tournament->setElitePrice1($updatedTournament->getElitePrice1());
-        }
-        if ($updatedTournament->getElitePrice2()) {
-            $tournament->setElitePrice2($updatedTournament->getElitePrice2());
-        }
-        if ($updatedTournament->getElitePrice3()) {
-            $tournament->setElitePrice3($updatedTournament->getElitePrice3());
-        }
-        if ($updatedTournament->getPriceSingle()) {
-            $tournament->setPriceSingle($updatedTournament->getPriceSingle());
-        }
-        if ($updatedTournament->getPriceDouble()) {
-            $tournament->setPriceDouble($updatedTournament->getPriceDouble());
-        }
-        if ($updatedTournament->getPriceMixed()) {
-            $tournament->setPriceMixed($updatedTournament->getPriceMixed());
-        }
-        if ($updatedTournament->getRegistrationClosingDate()) {
-            $tournament->setRegistrationClosingDate($updatedTournament->getRegistrationClosingDate());
-        }
-        if ($updatedTournament->getRandomDraw()) {
-            $tournament->setRandomDraw($updatedTournament->getRandomDraw());
-        }
-        if ($updatedTournament->getEmailContact()) {
-            $tournament->setEmailContact($updatedTournament->getEmailContact());
-        }
-        if ($updatedTournament->getTelContact()) {
-            $tournament->setTelContact($updatedTournament->getTelContact());
-        }
-        if ($updatedTournament->getRegistrationMethod()) {
-            $tournament->setRegistrationMethod($updatedTournament->getRegistrationMethod());
-        }
-        if ($updatedTournament->getPaymentMethod()) {
-            $tournament->setPaymentMethod($updatedTournament->getPaymentMethod());
-        }
-        if ($updatedTournament->getComment()) {
-            $tournament->setComment($updatedTournament->getComment());
-        }
+        if ($jsonReceived) {
+            $updatedTournament = $serializer->deserialize($jsonReceived, Tournament::class, "json");
 
-        if (in_array($tournament->getStartDate()->format("m"), ["09", "10", "11", "12"])) {
-            $tournament->setSeason("20" . $tournament->getStartDate()->format("y") . "/20" . $tournament->getStartDate()->format("y") + 1);
-        } else if (in_array($tournament->getStartDate()->format("m"), ["01", "02", "03", "04", "05", "06", "07", "08"])) {
-            $tournament->setSeason("20" . $tournament->getStartDate()->format("y") - 1 . "/20" . $tournament->getStartDate()->format("y"));
+            if ($updatedTournament->getName()) {
+                $tournament->setName($updatedTournament->getName());
+            }
+            if ($updatedTournament->getCity()) {
+                $tournament->setCity($updatedTournament->getCity());
+            }
+            if ($updatedTournament->getStartDate()) {
+                $tournament->setStartDate($updatedTournament->getStartDate());
+            }
+            if ($updatedTournament->getEndDate()) {
+                $tournament->setEndDate($updatedTournament->getEndDate());
+            }
+            if ($updatedTournament->getStandardPrice1()) {
+                $tournament->setStandardPrice1($updatedTournament->getStandardPrice1());
+            }
+            if ($updatedTournament->getStandardPrice2()) {
+                $tournament->setStandardPrice2($updatedTournament->getStandardPrice2());
+            }
+            if ($updatedTournament->getStandardPrice3()) {
+                $tournament->setStandardPrice3($updatedTournament->getStandardPrice1());
+            }
+            if ($updatedTournament->getElitePrice1()) {
+                $tournament->setElitePrice1($updatedTournament->getElitePrice1());
+            }
+            if ($updatedTournament->getElitePrice2()) {
+                $tournament->setElitePrice2($updatedTournament->getElitePrice2());
+            }
+            if ($updatedTournament->getElitePrice3()) {
+                $tournament->setElitePrice3($updatedTournament->getElitePrice3());
+            }
+            if ($updatedTournament->getPriceSingle()) {
+                $tournament->setPriceSingle($updatedTournament->getPriceSingle());
+            }
+            if ($updatedTournament->getPriceDouble()) {
+                $tournament->setPriceDouble($updatedTournament->getPriceDouble());
+            }
+            if ($updatedTournament->getPriceMixed()) {
+                $tournament->setPriceMixed($updatedTournament->getPriceMixed());
+            }
+            if ($updatedTournament->getRegistrationClosingDate()) {
+                $tournament->setRegistrationClosingDate($updatedTournament->getRegistrationClosingDate());
+            }
+            if ($updatedTournament->getRandomDraw()) {
+                $tournament->setRandomDraw($updatedTournament->getRandomDraw());
+            }
+            if ($updatedTournament->getEmailContact()) {
+                $tournament->setEmailContact($updatedTournament->getEmailContact());
+            }
+            if ($updatedTournament->getTelContact()) {
+                $tournament->setTelContact($updatedTournament->getTelContact());
+            }
+            if ($updatedTournament->getRegistrationMethod()) {
+                $tournament->setRegistrationMethod($updatedTournament->getRegistrationMethod());
+            }
+            if ($updatedTournament->getPaymentMethod()) {
+                $tournament->setPaymentMethod($updatedTournament->getPaymentMethod());
+            }
+            if ($updatedTournament->getComment()) {
+                $tournament->setComment($updatedTournament->getComment());
+            }
+
+            if (in_array($tournament->getStartDate()->format("m"), ["09", "10", "11", "12"])) {
+                $tournament->setSeason("20" . $tournament->getStartDate()->format("y") . "/20" . $tournament->getStartDate()->format("y") + 1);
+            } elseif (in_array($tournament->getStartDate()->format("m"), ["01", "02", "03", "04", "05", "06", "07", "08"])) {
+                $tournament->setSeason("20" . $tournament->getStartDate()->format("y") - 1 . "/20" . $tournament->getStartDate()->format("y"));
+            }
         }
 
         if (isset($uploadedFile)) {
@@ -230,7 +233,7 @@ class ApiTournamentController extends AbstractController
         }
 
         $repository->add($tournament, true);
-        return $this->json($tournament, 201);
+        return $this->json($tournament, 201, context: ["groups" => "tournament:read"]);
     }
 
     /**
